cc4ed70b48e68cbc14346be16bcef249eda337db
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2021-09-08 11:06:58 +0200

    Startat med Grunderna på CW och Speacialtecken

---
+++ Uppgift1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Hej\" hörde jag någon säga på stan.");
+
+            Console.WriteLine("Efter en stund svarade någon annan också med ett \"Hej!\".");
+        }
+    }
+}
---
+++ intro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Mitt namn är Linus");
+
+            Console.WriteLine("It is a long established fact\n" +
+             "that a reader will be distracted by the readable content of a page" + "when looking at its layout. The point of using Lorem Ipsum");
+
+            Console.WriteLine("I prog 1 läser vi C#");
+
+            Console.WriteLine("Mitt spel nick är\"Fungus\"");
+
+        }
+    }
+}
