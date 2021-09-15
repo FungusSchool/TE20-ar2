36fe2bdf5a998f3ae50ee27b50a2fd3c576a95d0
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2021-09-15 11:10:04 +0200

    Lucktext

---
+++ kapitel-2/LuckText/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LuckText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name one place: ");
+            string place = Console.ReadLine();
+            
+            Console.Write("Name one item: ");
+            string item = Console.ReadLine();
+            
+            Console.Write("Name one person: ");
+            string victim = Console.ReadLine();
+
+            Console.Write("Second place: ");
+            string body = Console.ReadLine();
+            
+            Console.Write("Second person: ");
+            string witness = Console.ReadLine();
+
+            Console.Write("Verb: ");
+            string witnessVerb = Console.ReadLine();
+            
+            Console.Write("Second verb: ");
+            string yourVerb = Console.ReadLine();
+            
+
+
+            Console.WriteLine($"You have just killed {victim} at {place} with (a) {item} and hid there body in {body}");
+            Console.WriteLine($"But {witness} ses you and {witnessVerb} in suprise!");
+            Console.WriteLine($"{witness} tries to run but you {yourVerb} so they stop. ");
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ kapitel-2/Övning/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Läser in ett namn! ");
+            string namn = Console.ReadLine();
+            Console.WriteLine("hej där " + namn);
+
+            Console.Write("Vart bor du? " );
+            string hem = Console.ReadLine();
+            Console.WriteLine("Varför vid " + hem);
+
+            Console.Write("Din ålder?");
+            string ålder = Console.ReadLine();
+            Console.WriteLine($"Du är {ålder} nu");
+        }
+    }
+}
