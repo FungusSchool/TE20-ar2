4fabbf49b03a5dcc61316e3da7c69e2fbc0a2595
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2021-09-22 11:08:19 +0200

    Vi har tränat på if satser och ToLower

---
+++ Kapitel-3/InteraktivBerättelse/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq.Expressions;
+using System;
+
+namespace InteraktivBerättelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // frågar om dom vill spela spelet
+            Console.WriteLine("Hej vill du spela spelet? ja eller nej");
+            string spela = Console.ReadLine().ToLower();
+
+            // Om dom säger Ja så fortsätter spelet annars avslutar det
+            if (spela == "ja") 
+            {
+                Console.WriteLine("Vad bra forsätt då!");
+
+                // Frågar om vilket håll dom vill gå och gör om det till små bokstäver,
+                Console.WriteLine("Vill du gå höger, vänster eller framåt");
+                string direction = Console.ReadLine().ToLower();
+                
+                // Vilket håll dom går bästemer vad som händer sen
+                if (direction == "höger")
+                {
+                    Console.WriteLine("Du svänger höger och börjar gå men ramlar i ett hål och dör av falet.");
+                    Console.WriteLine("GAME OVER!");
+
+                } else if (direction == "vänster")
+                {
+                    Console.WriteLine("Du svänger vänster och börja gå framåt. Du ser en hamburgare på marken.");
+                    Console.WriteLine("Vill du ta upp och äta den eller forsätta framåt? (Skriv Ät eller Gå)");
+                    string hamburgare = Console.ReadLine().ToLower();
+                    
+                    if (hamburgare == "ät")
+                    {
+                        Console.WriteLine("Du äter hamburgaren men börjar få ont i magen.");
+                    }
+                    else
+                    {
+                        
+                    }
+   
+                } else if (direction =="framåt")
+                {
+
+                }
+                //Om dom inte skriver nån av vallen så slutar programet med den texten. 
+                else 
+                {
+                    Console.WriteLine("Så kan du inte göra!");
+                }
+
+                
+            } else 
+            {
+
+                Console.WriteLine("Synd varför startade du ens programet men okej.");
+                
+            }
+            
+            
+            //väntar på ett input så inte fönstre stängs ner
+            Console.WriteLine("Tryck på valfri kanpp för att stänga programet!");
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
---
+++ Kapitel-3/Villkor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Villkor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Frågar om ålder
+            Console.Write("Hur gammal är du? (Heltal) ");
+            int Ålder = int.Parse(Console.ReadLine()); 
+
+            //om ålder är större än 18 "Du får ta körkort"
+            if (Ålder >= 18)
+            {
+                Console.WriteLine("Du får ta körkort!");
+            }
+
+            //Om du är 15 eller äldre = "Du får ta moped körkort!"
+            if (Ålder >= 15)
+            {
+             Console.WriteLine("Du får ta moped körkort!");  
+            }
+
+            //Fråga användare "Vad heter ABBAs senaste låt?
+            Console.Write("Vad heter ABBAs senaste Album? ");
+            string album = Console.ReadLine();
+            
+            //Är svaret korrekt
+            if (album == "Voyage")
+            {
+                Console.WriteLine("Rätt svar!");
+            }
+            else
+            {
+                Console.WriteLine("Fel svar! Det heter Voyage");
+            }
+
+            //Sista TP-frågan 
+            Console.Write("Vem missade straffen i matchen England-Frankrike? (efternamn) ");
+            //läs in och gör om till små bokstäver.
+            //Mbappe -> mbappe 
+            //mBappE -> mbappe
+            string spelare = Console.ReadLine().ToLower();
+            
+            if (spelare == "mbappe")
+            {
+                Console.WriteLine("Rätt svar!");
+            }
+            else
+            {
+                Console.WriteLine("Fel svar! Det var Mbappe");
+            }
+
+        }
+    }
+}
