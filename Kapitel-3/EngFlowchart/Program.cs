5eb136cb80e8317ba71a7a6a195c1b2bfe01212a
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2021-09-28 11:02:02 +0200

    Forstatt med if-saster

---
+++ Kapitel-3/EngFlowchart/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace EngFlowchart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Clear();
+            Console.WriteLine("Engineering Flowchart");
+
+            Console.Write("Does it move? (yes/no) ");
+            string answer1 = Console.ReadLine();
+            
+            if (answer1 == "yes")
+            {
+                Console.Write("Should it? (yes/no) ");
+                string answer2 = Console.ReadLine();
+                if (answer2 == "yes" )
+                {
+                    Console.WriteLine("No problem! 😃");
+                }
+                else
+                {
+                    Console.WriteLine("Use Duct Tape!   ");
+                }
+            }
+            else
+            {
+                Console.Write("Should it? (yes/no)  ");
+                string answer3 = Console.ReadLine();
+                
+                if (answer3 == "yes" )
+                {
+                    Console.WriteLine("use WD-40! ");
+                }
+                else
+                {
+                    Console.WriteLine("No problem! 😃");
+                }
+            }
+
+        }
+    }
+}
--- Kapitel-3/InteraktivBerättelse/Program.cs
+++ Kapitel-3/InteraktivBerättelse/Program.cs
@@ -17,7 +17,7 @@ namespace InteraktivBerättelse
                 Console.WriteLine("Vad bra forsätt då!");
 
                 // Frågar om vilket håll dom vill gå och gör om det till små bokstäver,
-                Console.WriteLine("Vill du gå höger, vänster eller framåt");
+                Console.WriteLine("Vill du gå höger, vänster eller framåt? (Skriv höger,vänster eller framåt)");
                 string direction = Console.ReadLine().ToLower();
                 
                 // Vilket håll dom går bästemer vad som händer sen
@@ -35,15 +35,62 @@ namespace InteraktivBerättelse
                     if (hamburgare == "ät")
                     {
                         Console.WriteLine("Du äter hamburgaren men börjar få ont i magen.");
+                        Console.WriteLine("Du går en bit till sen faller över och dör.");
+                        Console.WriteLine("GAME OVER!");
                     }
                     else
                     {
-                        
+                        Console.WriteLine("Du går förbi hamburgaren.");
+                        Console.WriteLine("Du kommer till ett hus. Det ser övergivet ut.");
+                        Console.WriteLine("Vill du försöka öppna framdörren eller gå runt huset först? (Skriv Dörren eller Gå)");
+                        //huset 1 för första valet vid huset
+                        string huset1 = Console.ReadLine().ToLower();
+
+                        if (huset1 == "dörren")
+                        {
+                            Console.WriteLine("Du öppnar fram dörren och blir skutten av ett givär rakt i huvud.");
+                            Console.WriteLine("Personen ville inte ha några gäster");
+                            Console.WriteLine("GAME OVER!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Du tar ett var runt huset men du ser inget. Det är mörkt i alla fönster. Fast du ser en bakdörr.");
+                            Console.WriteLine("Vill du ta bakdörren eller vill du testa framdörren? (Skriv Fram eller Bak) ");
+                            string huset2 = Console.ReadLine().ToLower();
+                            
+                            if (huset2 == "fram")
+                            {
+                            Console.WriteLine("Du öppnar framdörren och blir skutten av ett givär rakt i huvud.");
+                            Console.WriteLine("Personen ville inte ha några gäster");
+                            Console.WriteLine("GAME OVER!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Du öppnar bakdörren och får en kniv i halsen. ");
+                                Console.WriteLine("Personen ville inte ha några gäster");
+                                Console.WriteLine("GAME OVER!");
+                            }
+                        }
+                         
                     }
    
                 } else if (direction =="framåt")
                 {
-
+                    Console.WriteLine("Du börjar gå framåt. Du ser vad ser ut som en person ligga mot ett träd vid väggen.");
+                    Console.WriteLine("Vill du gå förbi eller stanna och inspektera saken? (Skriv Gå eller Inspektera)");
+                    string kroppen = Console.ReadLine().ToLower();
+                    
+                    if (kroppen == "gå")
+                    {
+                        Console.WriteLine("Du börjar gå förbi saken men så känner du ett slag i bakhuvdet.");
+                        Console.WriteLine("Det var en människa.");
+                        Console.WriteLine("GAME OVER!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du närmar dig formen som du nu ser är en människa. När du är tillräckligt nära känner du ett stick i foten. Du kollar ner och ser en spruta sitta i din fot. Det börjar snurra i ditt huvudt och du faller till marken medans allt blir mörkare. Det sista du ser är människan som ställer sig upp.");
+                        Console.WriteLine("GAME OVER!");
+                    }
                 }
                 //Om dom inte skriver nån av vallen så slutar programet med den texten. 
                 else 
