4480edff7517bc8fd88fec70cfb73cc074fcfe7b
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2021-10-13 11:06:24 +0200

    Provet kollat

---
+++ Programering/Kapitel-4/MenyProgram/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace MenyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enkelt menyprogram!");
+
+            //Skapa en slumgenerator
+            Random tärning = new.Random();
+
+
+            //Programloopen
+            while (true)
+            {
+                //Slumpa fram ett tal
+                int slumptal = tärning.Next(1, 4);
+
+                Console.WriteLine("Välj ett av följande alternativ? ");
+                Console.WriteLine("Väljer du sten? (1)");
+                Console.WriteLine("Väljer du sax? (2)");
+                Console.WriteLine("Väljer du påse? (3)");
+                Console.WriteLine("Vill du avsluta? (4)");
+                string valString = Console.ReadLine();
+
+                //Vill användaren avsluta?
+                if (valString == "4")
+                {
+                    break;
+                }
+
+                //Omvandla till en int för slumptal är en int
+                int valInt = int.Parse(valString);
+
+                //Är det rätt svar
+                if (valInt == 1 && slumptal == 1)
+                {
+                    Console.WriteLine("Oavgjort!");
+                }
+                else if (valInt == 2 && slumptal == 1)
+                {
+                    Console.WriteLine("Du förlora!");
+                }
+                else if (valInt == 3 && slumptal == 1)
+                {
+                    Console.WriteLine("Du vinner!");
+                }
+                else if (valInt == 1 && slumptal == 2)
+                {
+                    Console.WriteLine("Du vinner!");
+                }
+                else if (valInt == 2 && slumptal == 2)
+                {
+                    Console.WriteLine("Oavgjort!");
+                }
+                else if (valInt == 3 && slumptal == 2)
+                {
+                    Console.WriteLine("Du förlora!");
+                }
+                else if (valInt == 1 && slumptal == 3)
+                {
+                    Console.WriteLine("Du förlora!");
+                }
+                else if (valInt == 2 && slumptal == 3)
+                {
+                    Console.WriteLine("Du vinner!");
+                }
+                else if (valInt == 3 && slumptal == 3)
+                {
+                    Console.WriteLine("Oavgjort!");
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Programering/prover/prov1_genomgång/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace prov1_genomgång
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // berätta vad programet gör
+            Console.WriteLine("Uträkning av nettolön \n---------------------");
+
+            //upprepa 
+            while (true)
+            {
+
+                // Fråga efter namn
+                Console.Write("Vad heter du? ");
+                string namn = Console.ReadLine();
+
+                //Fråga efter bruttolön
+                Console.Write("Ange din bruttolön i kronor: ");
+                int bruttolön = int.Parse(Console.ReadLine());
+
+                //Kolla att villkoren är uppfyllda för bruttolön
+                if (bruttolön >= 10000 && bruttolön < 45000)
+                {
+                    //Fråga efter skattesats
+                    Console.Write("Ange din skattesats i procent: ");
+                    int skattesatsen = int.Parse(Console.ReadLine());
+
+                    //Kolla att villkoren är uppfyllda för skattesats
+                    if (skattesatsen > 10 && skattesatsen < 45)
+                    {
+                        //Räkna ut nettolön
+                        int nettolön = bruttolön * (100 - skattesatsen) / 100;
+
+                        Console.WriteLine($"{namn}, din lön efter skatt är {nettolön}");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Skaatesats måste vara mellan 10 och 450");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Bruttolön måste vara mellan 10000 och 45000");
+                }
+
+                //Vill du köra igen
+                Console.Write("Vill du köra igen?(j/n) ");
+                string svar = Console.ReadLine();
+                
+                if (svar == "n") // (svar != "j")
+                {
+                    break;
+                }
+
+            }
+
+
+        }
+    }
+}
