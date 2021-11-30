919242839f03c55a2fc2dc00a24f953578ac48ea
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2021-11-30 10:25:02 +0100

    TryParse

---
+++ Programering/Kapitel-5/TP-frågor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TP_frågor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Slumoa fram en fråga!");
+
+            //Namnge korten (array)
+            string [] korten = {
+                "När uppfördes Verdi?",                 //0
+                "Vem van fotbolls-EM 1984",             //1
+                "Vem skrev frankenstein?",              //2
+                "Vad är 5+5?",                          //3
+                "När stardade andra världs kriget?",    //4
+                "Vem är president i USA 2021?"          //5
+                };
+
+            //kasta träningen 
+            Random tärning = new Random();
+            int slumptal = tärning.Next(0, 6);  //0 till 5 
+
+            //skriver utslaget
+            Console.WriteLine($"Tärningen slog {slumptal}");
+
+            //Skriv ut fråga 
+            Console.WriteLine($"Frågan: {korten[slumptal]}");
+        }
+    }
+}
---
+++ Programering/Kapitel-6/TryParse/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Slump generator 
+            Random generator = new Random();
+
+            //Slumpa fram ett tal 
+            int slumptal = generator.Next(1,101); //mellan 1-100
+            int slumptal2 = generator.Next(1,11); //mellan 1.10
+            
+            //be spelaren att gissa ett tal 
+
+            
+            //Försök översätta det inmattade till ett tal
+            int gissningTal = 0;
+            bool korrekt = false;
+
+            //loop för att tvinga spelaren mata in något korekt
+            while (korrekt != true)
+            {
+                Console.Write("Gissa ett tal (1-100): ");
+                string gissning = Console.ReadLine();
+                korrekt = int.TryParse(gissning, out gissningTal);
+                
+            }
+
+            //Var gissning korrekt
+            if (gissningTal == slumptal)
+            {
+                Console.WriteLine("Du gissade rätt!");
+            }
+            else
+            {
+                Console.WriteLine("Du gissade fel!");
+
+            }
+        }
+    }
+}
