cd476e03b84e59849e2bbfc149c526d6c6986f56
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2022-02-01 11:07:31 +0100

    SnöFlingor

---
+++ Programering/Raylib/Snöfligor/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Snöflingor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialisering
+            //--------------------------------------------------------------------------------------
+            const int fönsterB = 800;
+            const int fönsterH = 600;
+
+            Raylib.InitWindow(fönsterB, fönsterH, "Snöflingor");
+            Raylib.SetTargetFPS(60);
+
+
+            // TODO: Infoga variabler och objekt här
+            // Slumpmotor
+            Random generator = new Random();
+
+            int mängd = 200;
+
+            // Skapa plats för många flingor
+            Rectangle[] flingor = new Rectangle[mängd];
+
+            //Rectangle snö = new Rectangle(0, 0, 10, 10);
+
+
+            // Fyll på med flingor
+            // Lägg ut dom slumpmässigt
+
+            for (int i = 0; i < flingor.Length; i++)
+            {/* 
+                int x = generator.Next(20, fönsterB - 20);
+                int y = generator.Next(0, fönsterH) - 700;*/
+                
+                int storlek = generator.Next(2, 7);
+
+                flingor[i] = new Rectangle(0, 0, storlek, storlek);
+                flingor[i].x = generator.Next(20, fönsterB - 20);
+                flingor[i].y = generator.Next(0, fönsterH) - fönsterH;
+            }
+
+            //array för farten dom faller. 
+            float[] speeds = new float[mängd];
+            for (int i = 0; i < speeds.Length; i++)
+            {
+                speeds[i] = generator.Next(100, 200)/100f;
+            }
+
+            //Placera ut snöflingan
+            //snö.x = generator.Next(20, fönsterB - 20);
+
+            //--------------------------------------------------------------------------------------
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                // Updatering
+                //----------------------------------------------------------------------------------
+                // TODO: Uppdatera variabler här
+                /* snö.y += 1;
+                if (snö.y > 600)
+                {
+                    snö.y = 0;
+                    snö.x = generator.Next(20, fönsterB - 20);
+                } */
+
+                for (int i = 0; i < flingor.Length; i++)
+                {
+                    flingor[i].y += speeds[i];
+
+                    if (flingor[i].y > 600)
+                    {
+                        flingor[i].x = generator.Next(20, fönsterB - 20);
+                        flingor[i].y = generator.Next(0, fönsterH) - fönsterH;
+                    }
+                }
+
+
+
+                //----------------------------------------------------------------------------------
+
+                // Rita
+                //----------------------------------------------------------------------------------
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DARKBLUE);
+
+                // Rita ut flingorna
+                for (int i = 0; i < flingor.Length; i++)
+                {
+                    Raylib.DrawRectangleRec(flingor[i], Color.WHITE);
+                }
+
+
+                //Raylib.DrawRectangleRec(snö, Color.WHITE);
+
+                Raylib.EndDrawing();
+                //----------------------------------------------------------------------------------
+            }
+        }
+    }
+}
---
+++ Programering/Raylib/Uppgift-8-9/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialisering
+            //--------------------------------------------------------------------------------------
+            const int fönsterB = 800;
+            const int fönsterH = 450;
+
+            Raylib.InitWindow(fönsterB, fönsterH, "raylib [cs]");
+            Raylib.SetTargetFPS(60);
+
+            // Game state variabler
+            float sek = 60;
+            string scen = "Scen 0";
+
+            // TODO: Infoga variabler och objekt här
+            //--------------------------------------------------------------------------------------
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                // Updatering
+                //----------------------------------------------------------------------------------
+                // TODO: Uppdatera variabler här
+                //----------------------------------------------------------------------------------
+                sek -= Raylib.GetFrameTime();
+
+                // Rita
+                //----------------------------------------------------------------------------------
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                if (scen == "Scen 0")
+                {
+                    // Rita ut objekt
+                    Raylib.DrawText(scen, 10, 10, 50, Color.RED);
+
+                    // Rita ut tiden
+                    Raylib.DrawText($"Tid {(int)sek}", 600, 0, 50, Color.RED);
+
+                    // Räkna ned tiden kvar
+                    if (sek <= 0)
+                    {
+                        scen = "Scen 1";
+                        sek = 45;
+                    }
+                }
+                else if (scen == "Scen 1")
+                {
+                    Raylib.DrawText(scen, 10, 10, 50, Color.RED);
+
+                    // Rita ut tiden
+                    Raylib.DrawText($"Tid {(int)sek}", 600, 0, 50, Color.RED);
+
+                    
+                    if (sek <= 0)
+                    {
+                        scen = "Slut";
+                    }
+                }
+                else if (scen == "Slut")
+                {
+                    Raylib.DrawText("Game Over!", 200, 200, 50, Color.RED);
+                }
+
+                Raylib.EndDrawing();
+                //----------------------------------------------------------------------------------
+            }
+        }
+    }
+}
