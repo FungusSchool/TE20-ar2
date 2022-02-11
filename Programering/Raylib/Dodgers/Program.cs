787806c0a16f6a1116c3473b828a60bde62f90ba
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2022-02-11 16:14:42 +0100

    yes

--- Programering/Raylib/Dodgers/Program.cs
+++ Programering/Raylib/Dodgers/Program.cs
@@ -5,22 +5,24 @@ namespace Dodgers
 {
     class Program
     {
+
+        static int fönsterB = 800;
+        static int fönsterH = 600;
+
+        static Random generator = new Random();
+        static Rectangle coin = new Rectangle(generator.Next(0, fönsterB - 50), generator.Next(-150, -50), 50, 50);
+        static Rectangle player = new Rectangle(100, fönsterH - 50, 200, 25);
+        static Rectangle enemy = new Rectangle(generator.Next(0, fönsterB - 50), generator.Next(-150, -50), 50, 50);
+
         static void Main(string[] args)
         {
             // Initialisering
             //--------------------------------------------------------------------------------------
-            const int fönsterB = 800;
-            const int fönsterH = 600;
 
             Raylib.InitWindow(fönsterB, fönsterH, "Snöflingor");
             Raylib.SetTargetFPS(60);
 
             // TODO: Infoga variabler och objekt här
-            Random generator = new Random();
-
-            Rectangle coin = new Rectangle(generator.Next(0, fönsterB - 50), generator.Next(-150, -50), 50, 50);
-            Rectangle player = new Rectangle(100, fönsterH - 50, 200, 25);
-            Rectangle enemy = new Rectangle(generator.Next(0, fönsterB - 50), generator.Next(-150, -50), 50, 50);
 
             int points = 0;
             int life = 3;
@@ -36,55 +38,11 @@ namespace Dodgers
                 //Time
                 time += Raylib.GetFrameTime();
                 //speed
-                if (time > speedTime)
-                {
-                    speedTime += 15;
-                    speed += 1;
-                }
-                //Coin
-                coin.y += speed;
-                if (coin.y > fönsterH)
-                {
-                    coin.y = generator.Next(-150, -50);
-                    coin.x = generator.Next(0, fönsterB);
-                }
-                //Enemy
-                enemy.y += speed;
-                if (enemy.y > fönsterH)
-                {
-                    enemy.y = generator.Next(-150, -50);
-                    enemy.x = generator.Next(0, fönsterB);
-                }
+                Speed(time, speedTime, speed);
                 //Input
-                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT) || Raylib.IsKeyDown(KeyboardKey.KEY_D))
-                {
-                    if (player.x < fönsterB - 200)
-                    {
-                        player.x += 5;
-                    }
-                }
-                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT) || Raylib.IsKeyDown(KeyboardKey.KEY_A))
-                {
-                    if (player.x > 0 )
-                    {
-                    player.x -= 5;
-                    }
-                }
+                Input();
                 //Kollision
-                //with Coin
-                if (Raylib.CheckCollisionRecs(player, coin))
-                {
-                    coin.y = generator.Next(-150, -50);
-                    coin.x = generator.Next(0, fönsterB);
-                    points += 1; 
-                }
-                //With Enemy
-                if (Raylib.CheckCollisionRecs(player, enemy))
-                {
-                    enemy.y = generator.Next(-150, -50);
-                    enemy.x = generator.Next(0, fönsterB);
-                    life -= 1; 
-                }
+                Kollision(ref points, ref life);
                 //----------------------------------------------------------------------------------
 
                 // Rita
@@ -101,6 +59,65 @@ namespace Dodgers
                 Raylib.EndDrawing();
                 //----------------------------------------------------------------------------------
             }
+        }
+
+        static void Input()
+        {
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT) || Raylib.IsKeyDown(KeyboardKey.KEY_D))
+            {
+                if (player.x < fönsterB - 200)
+                {
+                    player.x += 5;
+                }
+            }
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT) || Raylib.IsKeyDown(KeyboardKey.KEY_A))
+            {
+                if (player.x > 0)
+                {
+                    player.x -= 5;
+                }
+            }
+        }
+
+        static void Speed(float time, int speedTime, int speed)
+        {
+            if (time > speedTime)
+            {
+                speedTime += 15;
+                speed += 1;
+            }
+            //Coin
+            coin.y += speed;
+            if (coin.y > fönsterH)
+            {
+                coin.y = generator.Next(-150, -50);
+                coin.x = generator.Next(0, fönsterB);
+            }
+            //Enemy
+            enemy.y += speed;
+            if (enemy.y > fönsterH)
+            {
+                enemy.y = generator.Next(-150, -50);
+                enemy.x = generator.Next(0, fönsterB);
+            }
+        }
+
+        static void Kollision(ref int points, ref int life)
+        {
+            //with Coin
+                if (Raylib.CheckCollisionRecs(player, coin))
+                {
+                    coin.y = generator.Next(-150, -50);
+                    coin.x = generator.Next(0, fönsterB);
+                    points += 1;
+                }
+                //With Enemy
+                if (Raylib.CheckCollisionRecs(player, enemy))
+                {
+                    enemy.y = generator.Next(-150, -50);
+                    enemy.x = generator.Next(0, fönsterB);
+                    life -= 1;
+                }
         }
     }
 }
---
+++ Programering/ReadInt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ReadInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange ett tal: ");
+            int tal1, helTal;
+            while (int.TryParse(Console.ReadLine(), out helTal) == false)
+            {
+                Console.Write("Du skerv inte ett heltal: ");
+            }
+            tal1 = helTal;
+
+            Säkraheltal("skriv ett heltal: ");
+        }
+
+        static int Säkraheltal(string text) // skapar funktionen Säkratal
+            {
+                int tal; // skapar int tal
+                Console.Write(text);
+                while (!(int.TryParse(Console.ReadLine(), out tal))) // Tar vad användaren skriver och försöker göra det till en int, loopen fortsätter så länge det inte går att omvandla till int
+                {
+                    Console.WriteLine("Ett fel uppstod försök igen: ");
+                }
+                return tal; // ger tal som svar på funktionen
+            }
+    }
+}
