b618a57ff35889698d08529d23fab8f30bcddd08
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2022-04-06 11:02:43 +0200

    Method

---
+++ Programering/Övningar/Method/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] choices = {"Hej", "Hallo", "Si"};
+
+            GetChoice(choices); 
+
+        }
+
+        static void GetChoice(string[] choices)
+        {
+            
+            for (int i = 0; i < choices.Length; i++)
+            {
+                Console.WriteLine(choices[i] + (i+1));
+            }
+
+            bool correct = false;
+            while (correct == false)
+            {
+               int choice = SafeInts("Chose one: ");
+               
+               //Fungerar inte här
+                if (choice <= choices.Length+1 && choice < 0)
+                {
+                    correct = true;
+                }
+                else
+                {
+                  Console.WriteLine("Not a choice try again.");  
+                }
+            }
+        }
+        static int SafeInts(string text) // skapar funktionen Säkratal
+            {
+                int tal; // skapar int tal
+                Console.Write(text);
+                while (!(int.TryParse(Console.ReadLine(), out tal))) // Tar vad användaren skriver och försöker göra det till en int, loopen fortsätter så länge det inte går att omvandla till int
+                {
+                    Console.WriteLine("Ett fel uppstod försök igen");
+                }
+                return tal; // ger tal som svar på funktionen
+            }
+    }
+}
