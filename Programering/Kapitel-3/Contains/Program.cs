ef352be19cc20018a441be3f7818d2bc1d0bde68
Linus Strindin <Strindin.Linus@elev.ga.ntig.se>
2022-04-27 11:04:58 +0200

    g

---
+++ Programering/Kapitel-3/Contains/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Contains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in din epost: ");
+            string epost = Console.ReadLine();
+
+            if (epost.Contains("@"))
+            {
+                Console.WriteLine("Dyu har en giltig epost");
+            }
+
+            Console.WriteLine("Mata in ett mattetal: ");
+            string matteTal = Console.ReadLine();
+
+            //berätta om tallet inehåller + - / *
+            bool flagga = false;
+            if (matteTal.Contains("+"))
+            {
+                Console.WriteLine("Du använder operator +");
+                flagga = true;
+            }
+            if (matteTal.Contains("-"))
+            {
+                Console.WriteLine("Du använder operator -");
+                flagga = true;
+
+            }
+            if (matteTal.Contains("/"))
+            {
+                Console.WriteLine("Du använder operator /");
+                flagga = true;
+
+            }
+            if (matteTal.Contains("*"))
+            {
+                Console.WriteLine("Du använder operator *");
+                flagga = true;
+
+            }
+
+            if (!flagga)
+            {
+                Console.WriteLine("Du använder inte någin av +, -, *, /");
+            }
+        }
+    }
+}
